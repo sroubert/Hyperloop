04372b350fc5d46895cdc40f4ad16b13f1190d5c
Sebastian Roubert <sr949@cornell.edu>
2017-11-19 12:11:45 -0500

    forces for multiple max angles

--- torque.m
+++ torque.m
@@ -1,4 +1,4 @@
-maxAngle = 0.2*(pi/180); %rad
+maxAngle = linspace(0.01,0.2)*(pi/180); %rad
 timeResponse =0.1 ; %s
 
 %geometry
@@ -9,7 +9,7 @@ mass = 323*(0.453592);  %lbs to kg
 
 I = (1/12)*mass*(w^2 + l^2);
 
-alpha = 2*maxAngle/timeResponse^2;
+alpha = 2*maxAngle./timeResponse^2;
 
 torqueOutput = I*alpha
 
@@ -17,4 +17,4 @@ torqueOutput = I*alpha
 
 distFromCOM = 0.60325; %meters
 
-F = torqueOutput/distFromCOM
+F = torqueOutput./distFromCOM
